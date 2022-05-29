3be591bcced872abcf11bc6f502e15c4d6cb54a6
Rajan Bhattarai <cdrrazan@gmail.com>
2022-05-29 10:37:27 +0545

    WIP for new twitter trends

---
+++ app/services/data/twitter_trends/get_latest_trend_service.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require_relative '../../tweet_result_service'
+
+module AtonxTweetBot
+  class PostTweetService
+    def initialize
+    end
+
+    def perform
+    end
+end
+
+AtonxTweetBot::PostTweetService.new.perform
