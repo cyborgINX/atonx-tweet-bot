4ec006ac7a8b4cdd5d7967383255ba2e85274658
Rajan Bhattarai <cdrrazan@gmail.com>
2022-05-29 09:57:30 +0545

    Add example ruby file

---
+++ app/services/data/_example.rb
@@ -0,0 +1,24 @@
+# require libraries
+# require_relative '../../tweet_result_service'
+#
+# [ CONSTANTS ]
+#
+# module AtonxTweetBot
+#   class SampleClassName
+#     attr_reader 
+#
+#     def initialize
+#       ...
+#     end
+#
+#     def perform
+#       ... process your data
+#       TweetResultService.new.process(final_data)
+#     end
+#
+#     private
+#
+#     ... private methods collection
+# end
+#
+# AtonxTweetBot::SampleClassName.new.perform
